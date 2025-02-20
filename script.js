0cc4a0684cc8aa8ec40e3fb2cf8d5e868b25f028
myleslim <mousee1030@gmail.com>
2025-02-20 18:04:56 +0800

    added padStart to make it look neater

--- script.js
+++ script.js
@@ -31,5 +31,10 @@ function updateTimer(){
   let seconds = Math.floor((elapsedTime / 1000) % 60);
   let milliseconds = Math.floor(elapsedTime % 1000);
 
+  hours = String(hours).padStart(2, '0');
+  minutes = String(minutes).padStart(2, '0');
+  seconds = String(seconds).padStart(2, '0');
+  milliseconds = String(milliseconds).padStart(2, '0');
+
   display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
 }
